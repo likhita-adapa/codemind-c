13ea7b589979bc1b5a500aefb5b5335a9006b691
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-01-24 21:58:14 +0530

    #include<stdio.h> int main() {     int x,y;     scanf("%d%d",&x,&y);     if(x==0&&y%2==0)     {         printf("YES");     }     else if(x==0&&y%2!=0)     {         printf("NO");     }     else if((x+2*y)%2==0)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Can_share_equally?.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d%d",&x,&y);
+    if(x==0&&y%2==0)
+    {
+        printf("YES");
+    }
+    else if(x==0&&y%2!=0)
+    {
+        printf("NO");
+    }
+    else if((x+2*y)%2==0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
