ac4e719448106f65792e41af34d322b933fcc26f
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-01-27 14:30:23 +0530

    #include <stdio.h>
    int main()
    {
        int n,s=0,p=1,d;
        scanf("%d",&n);
        for(;n>0;)
        {
            d=n%10;
            s=s+d;
            p=p*d;
            n=n/10;
        }
        if(s==p)
            printf("Spy Number");
        else
            printf("Not Spy Number");
        return 0;
    }

---
+++ Spy_Number.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,s=0,p=1,d;
+    scanf("%d",&n);
+    for(;n>0;)
+    {
+        d=n%10;
+        s=s+d;
+        p=p*d;
+        n=n/10;
+    }
+    if(s==p)
+        printf("Spy Number");
+    else
+        printf("Not Spy Number");
+    return 0;
+}
