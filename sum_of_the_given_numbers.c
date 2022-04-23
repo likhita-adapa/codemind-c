30470b9c2d307540134567ff333cac092b5f0311
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-04-23 19:23:19 +0530

    #include<stdio.h> int main() {     int n,a,b,c,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d%d",&a,&b);         c=a+b;         printf("%d ",c);     }     return 0; }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,a,b,c,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&a,&b);
+        c=a+b;
+        printf("%d
+",c);
+    }
+    return 0;
+}
