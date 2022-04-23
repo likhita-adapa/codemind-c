bbd546e3995629449ce6a6d35fcce2b36d8b9f74
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-04-23 19:58:25 +0530

    #include<stdio.h> int main() {     int i;     scanf("%d",&i);     if(i==2)         printf("1");     else if(i==1)         printf("0");     else         printf("2");     return 0; }

---
+++ The_Pandemic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    if(i==2)
+        printf("1");
+    else if(i==1)
+        printf("0");
+    else
+        printf("2");
+    return 0;
+}
