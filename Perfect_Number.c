1fd1bde1dc69e3877bf71ba40ca75260855fcceb
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-04-29 12:40:10 +0530

    #include<stdio.h> int main() {     int n,i,rem,sum=0;     scanf("%d",&n);     for(i=1;i<n;i++)     {         rem=n%i; 	if(rem==0)         {             sum=sum+i;         }     }     if(sum==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Perfect_Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,rem,sum=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++)
+    {
+        rem=n%i;
+	if(rem==0)
+        {
+            sum=sum+i;
+        }
+    }
+    if(sum==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
