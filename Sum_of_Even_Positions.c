06f55533b8b97ce2182fa532a58a234a0c9bfae1
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-29 15:01:17 +0530

    #include<stdio.h> int main() {     int n,i,sum=0,a[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {        scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {        if(i%2==0)        {          sum=sum+a[i];        }     }     printf("%d",sum); }

---
+++ Sum_of_Even_Positions.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,sum=0,a[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+       scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       if(i%2==0)
+       {
+         sum=sum+a[i];
+       }
+    }
+    printf("%d",sum);
+}
