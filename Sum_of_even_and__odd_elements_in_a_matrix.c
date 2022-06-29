6ba6ea352ca6f45602fd7dab2259a0205d7c7a70
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-06-30 05:10:01 +0530

    #include<stdio.h> int main() {     int m,n;     scanf("%d %d",&m,&n);     int a[m][n],i,j,es=0,os=0;     for(i=0; i<m; i++)     {         for(j=0; j<n; j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0; i<m; i++)     {         for(j=0; j<n; j++)         {             if(a[i][j]%2==0)             {                 es=es+a[i][j];             }             else if(a[i][j]%2!=0)             {                 os=os+a[i][j];             }         }     }     printf("%d %d",es,os); }

---
+++ Sum_of_even_and__odd_elements_in_a_matrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int m,n;
+    scanf("%d %d",&m,&n);
+    int a[m][n],i,j,es=0,os=0;
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0; i<m; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            if(a[i][j]%2==0)
+            {
+                es=es+a[i][j];
+            }
+            else if(a[i][j]%2!=0)
+            {
+                os=os+a[i][j];
+            }
+        }
+    }
+    printf("%d %d",es,os);
+}
