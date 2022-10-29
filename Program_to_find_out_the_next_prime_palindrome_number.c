4b8097909644ce9703431a6374b24ac156e9e661
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-10-29 21:41:14 +0530

    #include<stdio.h> int main() {     int n,m,s,i,r,res=0,x;     scanf("%d",&m);     s=1;     for(n=m+1;n<=100000;n++)     {         s=1;         for(i=2;i<=n/2;i++)         {             if(n%i==0)             {                 s=0;                 break;             }         }         if(s==1)         {             res=0;             x=n;             while(x>0)             {                 r=x%10;                 res=(res*10)+r;                 x=x/10;             }             if(res==n)             {                 printf("%d",n);                 break;             }         }     } }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,s,i,r,res=0,x;
+    scanf("%d",&m);
+    s=1;
+    for(n=m+1;n<=100000;n++)
+    {
+        s=1;
+        for(i=2;i<=n/2;i++)
+        {
+            if(n%i==0)
+            {
+                s=0;
+                break;
+            }
+        }
+        if(s==1)
+        {
+            res=0;
+            x=n;
+            while(x>0)
+            {
+                r=x%10;
+                res=(res*10)+r;
+                x=x/10;
+            }
+            if(res==n)
+            {
+                printf("%d",n);
+                break;
+            }
+        }
+    }
+}
