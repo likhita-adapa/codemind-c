868ae7e45ca1b9f50b5cafab69de659a6f927ec1
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-11-02 20:23:22 +0530

    #include<stdio.h> int main() {     int n,i,arr[100];     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=n-1;i>=n/2;i--)     {         printf("%d ",arr[i]);     }     for(i=0;i<n/2;i++)     {         printf("%d ",arr[i]);     } }

---
+++ Half_List.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100];
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=n-1;i>=n/2;i--)
+    {
+        printf("%d ",arr[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
