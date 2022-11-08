478ab3c8c6e6541519f60ff9a4fe35aa38a08ccf
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-11-08 20:28:16 +0530

    #include<stdio.h> int main() {     int i,j,n;     scanf("%d",&n);     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {             printf("%c ",'A'-1 + i);         }         printf(" ");     }     return 0; }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,21 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n;
-    printf("");
+    int i,j,n;
     scanf("%d",&n);
-    for(int i=n;i>=1;i--)
+    for(i=n;i>=1;i--)
     {
-        for(int j=1;j<=i-1;j++)
+        for(j=1;j<=i;j++)
         {
-            printf(" ");
-        }
-        for(int k=1;k<=n;k++)
-        {
-            if(i==1 || i==n || k==1 || k==n)
-             printf("*");
-             else
-             printf(" ");
+            printf("%c ",'A'-1 + i);
         }
         printf("
 ");
