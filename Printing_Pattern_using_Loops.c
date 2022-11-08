33355f18218b8e34bc55a22c7b4619afdddd4b4c
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-11-08 14:47:18 +0530

    #include<stdio.h> int main() {     int i,j,n;     printf("");     scanf("%d",&n);
        for(i=n;i>1;i--)
        {
            for(j=n;j>=1;j--)
            
            {
                if(j>i)printf("%d ",j);
                else printf("%d ",i);
            }
            for(j=2;j<=n;j++)
            {
                if(j>i)printf("%d ",j);
                else printf("%d ",i);
            }
            printf("
    ");
        }
        for(i=1; i<=n; i++)
        {
            for(j=n;j>=1;j--)
            {
                if(j>i)printf("%d ",j);
                else printf("%d ",i);
            }
            for(j=2;j<=n;j++)
            {
                if(j>i)printf("%d ",j);
                else printf("%d ",i);
                
            }
            printf("
    ");
        }
    }

---
+++ Printing_Pattern_using_Loops.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("");
+    scanf("%d",&n);
+    
+    for(i=n;i>1;i--)
+    {
+        for(j=n;j>=1;j--)
+        
+        {
+            if(j>i)printf("%d ",j);
+            else printf("%d ",i);
+        }
+        for(j=2;j<=n;j++)
+        {
+            if(j>i)printf("%d ",j);
+            else printf("%d ",i);
+        }
+        printf("
+");
+    }
+    for(i=1; i<=n; i++)
+    {
+        for(j=n;j>=1;j--)
+        {
+            if(j>i)printf("%d ",j);
+            else printf("%d ",i);
+        }
+        for(j=2;j<=n;j++)
+        {
+            if(j>i)printf("%d ",j);
+            else printf("%d ",i);
+            
+        }
+        printf("
+");
+    }
+}
