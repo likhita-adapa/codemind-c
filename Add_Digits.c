a56e01dece7ca220c30f7245194bb8bafc30caa9
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-11-09 15:00:48 +0530

    #include<stdio.h>
    int digSum(int n)
    {
        int sum = 0;
        
        while(n > 0 || sum > 9)
        {
            if(n == 0)
            {
                n = sum;
                sum = 0;
            }
            sum += n % 10;
            n /= 10;
        }
        return sum;
    }
    int main()
    {
        int n ;
        scanf("%d",&n);
     printf("%d",digSum(digSum(n)));
        return 0;
    }

---
+++ Add_Digits.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+ 
+int digSum(int n)
+{
+    int sum = 0;
+    
+    while(n > 0 || sum > 9)
+    {
+        if(n == 0)
+        {
+            n = sum;
+            sum = 0;
+        }
+        sum += n % 10;
+        n /= 10;
+    }
+    return sum;
+}
+int main()
+{
+    int n ;
+    scanf("%d",&n);
+ printf("%d",digSum(digSum(n)));
+    return 0;
+}
+
