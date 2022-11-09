1a8e0d82700a3ecf143bd056898f4a9cbd3c532f
likhita-adapa <97425742+likhita-adapa@users.noreply.github.com>
2022-11-09 15:10:47 +0530

    #include<stdio.h> int main() {     int m,n,count=0,i,j;     scanf("%d",&m);     scanf("%d",&n);     for(i=m;i<=n;i++)     {         if(i==1)         {             continue;         }         else         {         count=0;         for(j=2;j<i;j++)         {             if(i%j==0)             {                 count++;             }         }         if(count==0)         {             printf("%d ",i);         }         }     } }

---
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,count=0,i,j;
+    scanf("%d",&m);
+    scanf("%d",&n);
+    for(i=m;i<=n;i++)
+    {
+        if(i==1)
+        {
+            continue;
+        }
+        else
+        {
+        count=0;
+        for(j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==0)
+        {
+            printf("%d
+",i);
+        }
+        }
+    }
+}
